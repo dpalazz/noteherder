b8a29e6a7bda13929e4554eb7dda837382a50111
Dana Palazzo <dana@fretless.com>
2018-04-24 15:08:12 -0400

    Save notes from the form

--- src/Main.js
+++ src/Main.js
@@ -14,7 +14,8 @@ const Main = (props) => {
         notes={props.notes}
         setCurrentNote={props.setCurrentNote} />
       <NoteForm 
-        currentNote={props.currentNote} />
+        currentNote={props.currentNote}
+        saveNote={props.saveNote} />
     </div>
   )
 }
