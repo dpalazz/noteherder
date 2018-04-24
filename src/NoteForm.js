f874339d706bf046216540d8f79afcba06eda451
Dana Palazzo <dana@fretless.com>
2018-04-24 10:37:01 -0400

    Add Sidebar, NoteList, and NoteForm components

---
+++ src/NoteForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './NoteForm.css'
+
+class NoteForm extends Component {
+  render() {
+    return (
+      <div class="NoteForm">
+      NoteForm
+      </div>
+    )
+  }
+}
+
+export default NoteForm
---
+++ src/NoteList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './NoteList.css'
+
+const NoteList = () => {
+  return (
+    <div className="NoteList">
+    NoteList
+    </div>
+  )
+}
+
+export default NoteList
