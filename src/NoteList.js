cd1d42be74a1d4e133acd0c48dd5d85ec85b8e8c
Dana Palazzo <dana@fretless.com>
2018-04-24 11:31:32 -0400

    Dynamically render notes from an array

--- src/NoteList.js
+++ src/NoteList.js
@@ -4,17 +4,26 @@ import './NoteList.css'
 import Note from './Note'
 
 const NoteList = () => {
-  const note = {
+  const notes = [
+    {
     id: 'note-4',
     title: 'My fancy note',
     body: 'Oh so fancy, I do declare!'
-  }
+    },
+    {
+      id: 'note-5',
+      title: 'Another one',
+      body: 'Also very fancy'
+      },
+  ]
 
   return (
     <div className="NoteList">
       <h3>Notes</h3>
       <ul id="notes">
-        <Note note={note}/>
+      {
+        notes.map(note => <Note note={note}/>)
+      }
       </ul>
     </div>
   )
