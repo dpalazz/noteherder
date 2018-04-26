0d8fecb1753f6a94f80a649bbd9e15ba6136a938
Dana Palazzo <dana@fretless.com>
2018-04-26 11:07:45 -0400

    Sync notes with Firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import base from './base'
 
 import './App.css'
 import Main from './Main'
@@ -13,6 +14,16 @@ class App extends Component {
     }
   }
   
+  componentDidMount = () => {
+    base.syncState(
+      'notes', 
+      {
+        context: this,
+        state: 'notes'
+      }
+    )
+  }
+
   blankNote = () => {
     return {
       id: null,
@@ -41,7 +52,7 @@ class App extends Component {
 
   removeCurrentNote = () => {
     const notes = {...this.state.notes}
-    delete notes[this.state.currentNote.id]
+    notes[this.state.currentNote.id] = null
     this.setState({notes})
     this.resetCurrentNote()
   }
