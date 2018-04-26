dda87d91ce803e6f94fe016de137d2ddaf02223d
Dana Palazzo <dana@fretless.com>
2018-04-26 11:59:24 -0400

    Add Google authentication via Firebase

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
+import { googleProvider, auth } from './base'
 import './SignIn.css'
 
 const SignIn = ({ handleAuth }) => {
+
+  const authenticate = () => {
+    auth
+    .signInWithPopup(googleProvider)
+    .then(handleAuth)
+  }
+
   return(
     <div className="SignIn">
       <h1>SIGN IN NOW</h1>
-      <button onClick={handleAuth}>Sign In</button>
+      <button onClick={authenticate}>Sign In</button>
     </div>
   )
 }
