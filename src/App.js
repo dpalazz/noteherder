0ed821bcdd9a2f4cd863074d0e32adfa1987e73a
Dana Palazzo <dana@fretless.com>
2018-04-27 14:19:22 -0400

    Remove unused currentnote

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ class App extends Component {
 
     this.state = {
       notes: {},
-      currentNoteId: null,
       uid: null,
     }
   }
@@ -44,14 +43,6 @@ class App extends Component {
         state: 'notes'
       }
     )
-  }
-
-  setCurrentNote = (note) => {
-    this.setState({ currentNoteId: note.id })
-  }
-
-  resetCurrentNote = () => {
-    this.setCurrentNote({ id: null })
   }
 
   saveNote = (note) => {
@@ -98,7 +89,6 @@ class App extends Component {
       uid: null,
       notes: {}
     })
-    this.resetCurrentNote() 
   }
 
   signOut = () => { 
@@ -110,11 +100,6 @@ class App extends Component {
       saveNote: this.saveNote,
       removeNote: this.removeNote,
       signOut: this.signOut,
-    }
-
-    const noteData = {
-      notes: this.state.notes,
-      currentNoteId: this.state.currentNoteId,
     }
 
     return (
@@ -133,7 +118,7 @@ class App extends Component {
               this.signedIn()
                 ? <Main 
                     {...actions}
-                    {...noteData} />
+                    notes={this.state.notes} />
                 : <Redirect to='/sign-in' />
             )} />
             <Route render={() => <Redirect to='/notes'/>} />
