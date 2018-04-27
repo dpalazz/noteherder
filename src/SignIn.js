c107cab3f7ea82e3c99dfa07390dfda31fbaa803
Dana Palazzo <dana@fretless.com>
2018-04-27 10:44:30 -0400

    Add GitHub authentication

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-import { googleProvider, auth } from './base'
+import { googleProvider, githubProvider, auth } from './base'
 import './SignIn.css'
 import quill from './quill.svg'
 import googleLogo from './google.svg'
 
 const SignIn = () => {
 
-  const authenticate = () => {
-    auth.signInWithPopup(googleProvider)
+  const authenticate = (provider) => {
+    auth.signInWithPopup(provider)
   }
 
   return(
@@ -21,8 +21,14 @@ const SignIn = () => {
         <h3>Hey, Nerd! You Like Notes?</h3>
         <p>You never know when you'll need to write crap down. In fact, you should probably be taking notes right now.</p>
         <button 
+          className="github"
+          onClick={() => authenticate(githubProvider)} >
+          <i className="fa fa-github"></i>
+          Sign in with Github
+        </button>
+        <button 
           className="google"
-          onClick={authenticate} >
+          onClick={() => authenticate(googleProvider)} >
           <img src={googleLogo} alt="" />
           Sign in with Google
         </button>
--- src/base.example.js
+++ src/base.example.js
@@ -13,6 +13,7 @@ const app = firebase.initializeApp({
 })
 
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const githubProvider = new firebase.auth.GithubAuthProvider();
 export const auth = app.auth()
 
 export default Rebase.createClass(app.database())
